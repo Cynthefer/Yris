abb22063b56b157b6b9d89bc4a2859e0c0cb18cf
Cynthefer <cynthefer@gmail.com>
2026-05-16 21:04:10 +0200

    Refactor source files

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -18,41 +18,5 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-=======
->>>>>>> origin/main
-#include <../include/hardware.h>
-
-#include <iostream>
-#include <fstream>
-#include <string>
-
-bool getMemoryStats(long &Total_Ram, long &Avail_Ram, long &Used_Ram) {
-    std::ifstream meminfo("/proc/meminfo");
-    if (!meminfo.is_open()) {
-        return false;
-    }
-
-    std::string key, unit;
-    long value;
-
-    long memTotal = 0;
-    long memAvailable = 0;
-
-    while (meminfo >> key >> value >> unit) {
-        if (key == "MemTotal:") {
-            memTotal = value;           
-        }
-        else if (key == "MemAvailable:") {
-            memAvailable = value;       
-        }
-        if (memTotal && memAvailable)
-            break;
-    }
-
-    // Convert from kB to MB
-    Total_Ram = memTotal / 1024;
-    Avail_Ram = memAvailable / 1024;
-    Used_Ram  = Total_Ram - Avail_Ram;
-
-    return true;
-}
+//headers in the include directory
+#include <hardware.h>
