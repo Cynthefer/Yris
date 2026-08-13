adac85739485748587cc625b5610cb4a52733d7a
Cynthe'fer <cynthefer@gmail.com>
2026-08-13 22:03:36 +0200

    -continue in local environment

---
+++ include/yris.h
@@ -0,0 +1,32 @@
+    /*
+    Yris ; a commandline tool that displays hardware and software information
+    Copyright (C) 2026  Ntando Khanyile
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#pragma once
+
+#ifndef YRIS_H
+#define YRIS_H
+#endif
+
+#include "user.h"
+#include "system.h"
+#include "memory.h"
+#include "network.h"
+
+namespace yris{
+    
+}
